70ec05f3e124135b7deebf19876af006cb0d2bd0
HARSH DUNGER SINGH MEHRA <mehraharsh019@gmail.com>
2025-11-15 18:21:26 +0530

    Code logic improvement

--- .config/hypr/custom/hyprlock/media-info.sh
+++ .config/hypr/custom/hyprlock/media-info.sh
@@ -2,9 +2,7 @@
 
 status=$(playerctl status 2>/dev/null)
 
-if [ "$status" = "Playing" ]; then
-    playerctl metadata --format '{{ title }} - {{ artist }} | {{ playerName }}' 2>/dev/null || echo ''
-elif [ "$status" = "Paused" ]; then
+if [ "$status" ]; then
     playerctl metadata --format '{{ title }} - {{ artist }} | {{ playerName }}' 2>/dev/null || echo ''
 else
     echo ''
