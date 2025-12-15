c04dabe8cd19e0b607ea1c144dc8142fd18e7140
HARSH DUNGER SINGH MEHRA <mehraharsh019@gmail.com>
2025-12-15 19:20:04 +0530

    Remove irrelevant files

--- .config/hypr/custom/hyprlock/media-info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-status=$(playerctl status 2>/dev/null)
-
-if [ "$status" ]; then
-    playerctl metadata --format '{{ title }} - {{ artist }} | {{ playerName }}' 2>/dev/null || echo ''
-else
-    echo ''
-fi
